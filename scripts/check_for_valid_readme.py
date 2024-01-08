d5fdca90ccb2a75435e9d0f06abcb2f9f060592d
Thomas Pierce <thp@amazon.com>
2024-01-08 11:49:28 -0800

    Apply header to files missing it

--- scripts/check_for_valid_readme.py
+++ scripts/check_for_valid_readme.py
@@ -1,3 +1,5 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
 """Test script to check given paths for valid README.rst files."""
 import argparse
 import sys
