1d11ca3eceb90af78af859fa56712681aa877a78
Mohamed Asaker <asakem@amazon.com>
2024-01-12 11:51:32 -0800

    removed auto generated code

--- sample-applications/vehicle-dealership-sample-app/ImageServiceApp/ImageServiceApp/urls.py
+++ sample-applications/vehicle-dealership-sample-app/ImageServiceApp/ImageServiceApp/urls.py
@@ -14,10 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('images/', include("MainService.urls")),
-    path('admin/', admin.site.urls),
+    path('images/', include("MainService.urls"))
 ]
