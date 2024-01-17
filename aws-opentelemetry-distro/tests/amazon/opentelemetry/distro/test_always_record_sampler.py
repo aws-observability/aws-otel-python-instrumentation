66cfab21ea4f9e56268f2a0988aaba738aaef6dc
ADOT Patch workflow <adot-patch-workflow@github.com>
2024-01-17 15:52:33 -0800

    Fix based on comments, and fill the tests

--- aws-opentelemetry-distro/tests/amazon/opentelemetry/distro/test_always_record_sampler.py
+++ aws-opentelemetry-distro/tests/amazon/opentelemetry/distro/test_always_record_sampler.py
@@ -13,19 +13,25 @@ from opentelemetry.util.types import Attributes
 
 class TestAlwaysRecordSampler(TestCase):
     def setUp(self):
-        self.static_sampler: Sampler = StaticSampler(Decision.DROP)
         self.mock_sampler: Sampler = MagicMock()
         self.sampler: Sampler = AlwaysRecordSampler(self.mock_sampler)
 
     def test_get_description(self):
-        test_sampler: Sampler = AlwaysRecordSampler(self.static_sampler)
-        self.assertIn("AlwaysRecordSampler", test_sampler.get_description())
+        static_sampler: Sampler = StaticSampler(Decision.DROP)
+        test_sampler: Sampler = AlwaysRecordSampler(static_sampler)
+        self.assertEqual("AlwaysRecordSampler{AlwaysOffSampler}", test_sampler.get_description())
 
     def test_record_and_sample_sampling_decision(self):
         self.validate_should_sample(Decision.RECORD_AND_SAMPLE, Decision.RECORD_AND_SAMPLE)
 
+    def test_record_only_sampling_decision(self):
+        self.validate_should_sample(Decision.RECORD_ONLY, Decision.RECORD_ONLY)
+
+    def test_drop_sampling_decision(self):
+        self.validate_should_sample(Decision.DROP, Decision.RECORD_AND_SAMPLE)
+
     def validate_should_sample(self, root_decision: Decision, expected_decision: Decision):
-        root_result: SamplingResult = self.sampling_result(root_decision)
+        root_result: SamplingResult = _build_root_sampling_result(root_decision)
         self.mock_sampler.should_sample.return_value = root_result
         actual_result: SamplingResult = self.sampler.should_sample(
             parent_context=Context(),
@@ -41,17 +47,17 @@ class TestAlwaysRecordSampler(TestCase):
             self.assertEqual(actual_result.decision, root_decision)
         else:
             self.assertNotEqual(actual_result, root_result)
-            self.assertNotEqual(actual_result.decision, root_decision)
+            self.assertNotEqual(actual_result.decision, expected_decision)
 
         self.assertEqual(actual_result.attributes, root_result.attributes)
         self.assertEqual(actual_result.trace_state, root_result.trace_state)
 
-    @staticmethod
-    def sampling_result(sampling_decision: Decision):
-        sampling_attr: Attributes = {"key": sampling_decision.name}
-        sampling_trace_state: TraceState = TraceState()
-        sampling_trace_state.add("key", sampling_decision.name)
-        sampling_result: SamplingResult = SamplingResult(
-            decision=sampling_decision, attributes=sampling_attr, trace_state=sampling_trace_state
-        )
-        return sampling_result
+
+def _build_root_sampling_result(sampling_decision: Decision):
+    sampling_attr: Attributes = {"key": sampling_decision.name}
+    sampling_trace_state: TraceState = TraceState()
+    sampling_trace_state.add("key", sampling_decision.name)
+    sampling_result: SamplingResult = SamplingResult(
+        decision=sampling_decision, attributes=sampling_attr, trace_state=sampling_trace_state
+    )
+    return sampling_result
