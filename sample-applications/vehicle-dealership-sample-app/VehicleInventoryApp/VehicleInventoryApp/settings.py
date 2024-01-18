b9100c0d2d55423835bda9e84a1f5e929b7f03ae
Mohamed Asaker <asakem@amazon.com>
2024-01-17 21:30:01 -0800

    updated docker compose for local testing

--- sample-applications/vehicle-dealership-sample-app/VehicleInventoryApp/VehicleInventoryApp/settings.py
+++ sample-applications/vehicle-dealership-sample-app/VehicleInventoryApp/VehicleInventoryApp/settings.py
@@ -17,8 +17,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DEBUG = True
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
--- sample-applications/vehicle-dealership-sample-app/local_script.sh
+++ sample-applications/vehicle-dealership-sample-app/local_script.sh
@@ -19,6 +19,7 @@ fi
 
 rm VehicleInventoryApp/.env
 rm ImageServiceApp/.env
+rm .env
 
 echo "MYSQL_ROOT_PASSWORD=${password}" >> VehicleInventoryApp/.env
 echo "MYSQL_DATABASE=vehicle_inventory" >> VehicleInventoryApp/.env
@@ -26,7 +27,7 @@ echo "MYSQL_USER=djangouser" >> VehicleInventoryApp/.env
 echo "MYSQL_PASSWORD=${password}" >> VehicleInventoryApp/.env
 echo "DB_SERVICE_HOST=db" >> VehicleInventoryApp/.env
 echo "DB_SERVICE_PORT=3306" >> VehicleInventoryApp/.env
-echo "IMAGE_BACKEND_SERVICE_HOST=0.0.0.0" >> VehicleInventoryApp/.env
+echo "IMAGE_BACKEND_SERVICE_HOST=image-service-backend" >> VehicleInventoryApp/.env
 echo "IMAGE_BACKEND_SERVICE_PORT=8000" >> VehicleInventoryApp/.env
 
 echo "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" >> ImageServiceApp/.env
@@ -34,6 +35,9 @@ echo "AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}" >> ImageServiceApp/.env
 echo "AWS_SESSION_TOKEN=${AWS_SESSION_TOKEN}" >> ImageServiceApp/.env
 echo "S3_BUCKET=${s3_bucket}" >> ImageServiceApp/.env
 
+echo "MYSQL_ROOT_PASSWORD=${password}" >> .env
+echo "MYSQL_DATABASE=vehicle_inventory" >> .env
+echo "MYSQL_USER=djangouser" >> .env
+echo "MYSQL_PASSWORD=${password}" >> .env
 
-#docker compose -f VehicleInventoryApp/docker-compose.yaml up --build&
-#docker compose -f ImageServiceApp/docker-compose.yaml up --build&
+docker-compose up --build
