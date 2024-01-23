ab97f260947ea12b723612202142c71a9e44fa8b
ADOT Patch workflow <adot-patch-workflow@github.com>
2024-01-23 11:54:55 -0800

    init

--- aws-opentelemetry-distro/tests/amazon/opentelemetry/distro/test_aws_metric_attributes_span_exporter.py
+++ aws-opentelemetry-distro/tests/amazon/opentelemetry/distro/test_aws_metric_attributes_span_exporter.py
@@ -4,6 +4,7 @@ from typing import Optional
 from unittest import TestCase
 from unittest.mock import MagicMock, call
 
+from amazon.opentelemetry.distro._aws_span_processing_util import should_generate_dependency_metric_attributes, should_generate_service_metric_attributes
 from amazon.opentelemetry.distro.aws_metric_attributes_span_exporter import AwsMetricAttributesSpanExporter
 from opentelemetry.sdk.trace import ReadableSpan
 from opentelemetry.sdk.trace.export import ConsoleSpanExporter
@@ -28,7 +29,7 @@ def build_span_attributes(contains_attribute):
 def build_metric_attributes(contains_attribute):
     attribute: Attributes = {}
     if contains_attribute:
-        attribute["new key"] =  "new value"
+        attribute["new key"] = "new value"
     return attribute
 
 
@@ -38,6 +39,15 @@ def build_readable_span_mock(span_attributes: Attributes):
     mock_span_data.kind = SpanKind.SERVER
     mock_span_data.parent = None
     return mock_span_data
+
+
+def configure_mock_for_export(span_data_mock: ReadableSpan, metric_attributes: Attributes):
+    attribute_map: Attributes = {}
+    if should_generate_service_metric_attributes(span_data_mock):
+        attribute_map[SERVICE_METRIC] = metric_attributes
+    if should_generate_dependency_metric_attributes(span_data_mock):
+        attribute_map[DEPENDENCY_METRIC] = metric_attributes
+    return attribute_map
 
 
 class TestAwsMetricAttributesSpanExporter(TestCase):
