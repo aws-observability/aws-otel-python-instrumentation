dfa6c47a2f274bb441637d299c4032a77818b536
XinRan Zhang <xinranzh@amazon.com>
2024-01-27 10:49:01 -0800

    Code Style Fix

--- aws-opentelemetry-distro/tests/amazon/opentelemetry/distro/test_aws_metric_attributes_span_exporter.py
+++ aws-opentelemetry-distro/tests/amazon/opentelemetry/distro/test_aws_metric_attributes_span_exporter.py
@@ -328,7 +328,7 @@ class TestAwsMetricAttributesSpanExporter(TestCase):
         )
 
     def _configure_mock_for_export_with_multiple_side_effect(
-            self, span_data_mocks: [ReadableSpan], metric_attributes_list: [Attributes]
+        self, span_data_mocks: [ReadableSpan], metric_attributes_list: [Attributes]
     ):
         attributes_map_list: list = []
         for span in span_data_mocks:
