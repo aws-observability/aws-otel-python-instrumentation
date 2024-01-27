d17126e672d81cf1281f440f769d00b034129363
XinRan Zhang <xinranzh@amazon.com>
2024-01-26 16:44:20 -0800

    Add back a basic go-over test for processor builder

--- aws-opentelemetry-distro/tests/amazon/opentelemetry/distro/test_attribute_propagating_span_processor_builder.py
+++ aws-opentelemetry-distro/tests/amazon/opentelemetry/distro/test_attribute_propagating_span_processor_builder.py
@@ -1,30 +1,13 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 from unittest import TestCase
-from unittest.mock import MagicMock
 
-from amazon.opentelemetry.distro.attribute_propagating_span_processor import AttributePropagatingSpanProcessor
 from amazon.opentelemetry.distro.attribute_propagating_span_processor_builder import (
     AttributePropagatingSpanProcessorBuilder,
 )
-from opentelemetry.sdk.trace import ReadableSpan
 
 
 class TestAttributePropagatingSpanProcessorBuilder(TestCase):
     def test_basic(self):
         builder: AttributePropagatingSpanProcessorBuilder = AttributePropagatingSpanProcessorBuilder()
-        span_mock: ReadableSpan = MagicMock()
-        span_mock._attributes = {"key": "value"}
-
-        def extractor(span: ReadableSpan) -> str:
-            return next(iter(span._attributes.values()))
-
         self.assertIs(builder.set_propagation_data_key("test"), builder)
-
-        builder.set_attributes_keys_to_propagate(["test key"])
-        builder.set_propagation_data_extractor(extractor)
-
-        processor: AttributePropagatingSpanProcessor = builder.build()
-        self.assertEqual(processor._attribute_keys_to_propagate, ("test key",))
-        self.assertEqual(processor._propagation_data_key, "test")
-        self.assertEqual(processor._propagation_data_extractor(span_mock), "value")
---
+++ aws-opentelemetry-distro/tests/amazon/opentelemetry/distro/test_aws_span_metrics_processor_builder.py
@@ -0,0 +1,23 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+from unittest import TestCase
+from unittest.mock import MagicMock
+
+from opentelemetry.sdk.metrics import MeterProvider
+
+from amazon.opentelemetry.distro.aws_span_metrics_processor import AwsSpanMetricsProcessor
+from amazon.opentelemetry.distro.aws_span_metrics_processor_builder import AwsSpanMetricsProcessorBuilder
+from amazon.opentelemetry.distro.metric_attribute_generator import MetricAttributeGenerator
+
+
+class TestAwsSpanMetricsProcessorBuilder(TestCase):
+    def test_basic(self):
+        meter_provider: MeterProvider = MeterProvider()
+        builder: AwsSpanMetricsProcessorBuilder = AwsSpanMetricsProcessorBuilder(meter_provider, None)
+        self.assertIs(builder.set_scope_name("test"), builder)
+
+        generator_mock: MetricAttributeGenerator = MagicMock()
+        builder.set_generator(generator_mock)
+        builder.set_scope_name("test scope name")
+        metric_processor: AwsSpanMetricsProcessor = builder.build()
+        self.assertEqual(metric_processor._error_histogram.record(1, None), None)
