e2adee061705d99d5be736821a8e5d30230e3803
ADOT Patch workflow <adot-patch-workflow@github.com>
2024-01-30 11:49:39 -0800

    Chkstyle fix

--- aws-opentelemetry-distro/tests/amazon/opentelemetry/distro/test_attribute_propagating_span_processor.py
+++ aws-opentelemetry-distro/tests/amazon/opentelemetry/distro/test_attribute_propagating_span_processor.py
@@ -37,9 +37,9 @@ class TestAttributePropagatingSpanProcessor(TestCase):
 
     def test_attributes_propagation_by_spankind(self):
         for span_kind in SpanKind:
-            span_with_app_only: Span = self.tracer.start_span(name="parent", kind=span_kind, attributes={_TEST_KEY_1: "TestValue1"})
-            span_with_op_only: Span = self.tracer.start_span(name="parent", kind=span_kind, attributes={_TEST_KEY_2: "TestValue2"})
-            span_with_app_and_op: Span = self.tracer.start_span(name="parent", kind=span_kind, attributes={_TEST_KEY_1: "TestValue1", _TEST_KEY_2: "TestValue2"})
+            span_with_app_only: Span = self.tracer.start_span(name="parent", kind=span_kind.value, attributes={_TEST_KEY_1: "TestValue1"})
+            span_with_op_only: Span = self.tracer.start_span(name="parent", kind=span_kind.value, attributes={_TEST_KEY_2: "TestValue2"})
+            span_with_app_and_op: Span = self.tracer.start_span(name="parent", kind=span_kind.value, attributes={_TEST_KEY_1: "TestValue1", _TEST_KEY_2: "TestValue2"})
 
             if span_kind == SpanKind.SERVER:
                 self._validate_span_attributes_inheritance(span_with_app_only, "parent", None, None)
