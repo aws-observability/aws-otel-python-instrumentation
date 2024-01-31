3081f5f51f0653f3365c5913160cfc3d62326f9f
XinRan Zhang <xinranzh@amazon.com>
2024-01-30 20:48:01 -0800

    Functional finished

--- aws-opentelemetry-distro/tests/amazon/opentelemetry/distro/test_attribute_propagating_span_processor.py
+++ aws-opentelemetry-distro/tests/amazon/opentelemetry/distro/test_attribute_propagating_span_processor.py
@@ -1,6 +1,5 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
-from trace import Trace
 from typing import Callable, Optional
 from unittest import TestCase
 
