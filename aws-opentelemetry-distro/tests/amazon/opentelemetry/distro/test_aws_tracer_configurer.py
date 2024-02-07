3592f9c573fd42d6df9d0d431b6f9426da23fe0c
ADOT Patch workflow <adot-patch-workflow@github.com>
2024-02-07 09:43:56 -0800

    Fix based on comment

--- aws-opentelemetry-distro/tests/amazon/opentelemetry/distro/test_aws_tracer_configurer.py
+++ aws-opentelemetry-distro/tests/amazon/opentelemetry/distro/test_aws_tracer_configurer.py
@@ -14,32 +14,37 @@ from opentelemetry.sdk.trace.export.in_memory_span_exporter import InMemorySpanE
 from opentelemetry.trace import get_tracer_provider
 
 
+# This class setup Tracer Provider Globally, which can only set once
+# if there is another setup for tracer provider, may cause issue
 class TestAwsTracerConfigurer(TestCase):
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         os.environ.setdefault(OTEL_TRACES_EXPORTER, "none")
         os.environ.setdefault(OTEL_METRICS_EXPORTER, "none")
         os.environ.setdefault(OTEL_LOGS_EXPORTER, "none")
         os.environ.setdefault(OTEL_TRACES_SAMPLER, "traceidratio")
         os.environ.setdefault(OTEL_TRACES_SAMPLER_ARG, "0.01")
-        self.span_exporter: InMemorySpanExporter = InMemorySpanExporter()
-        self.simple_span_processor: SimpleSpanProcessor = SimpleSpanProcessor(self.span_exporter)
-        self.aws_otel_configurator: AwsOpenTelemetryConfigurator = AwsOpenTelemetryConfigurator()
-        self.aws_otel_configurator.configure()
-        self.tracer_provider: TracerProvider = get_tracer_provider()
-        self.aws_open_telemetry_distro: AwsOpenTelemetryDistro = AwsOpenTelemetryDistro()
-        self.aws_open_telemetry_distro.configure()
+        aws_open_telemetry_distro: AwsOpenTelemetryDistro = AwsOpenTelemetryDistro()
+        aws_open_telemetry_distro.configure()
+        aws_otel_configurator: AwsOpenTelemetryConfigurator = AwsOpenTelemetryConfigurator()
+        aws_otel_configurator.configure()
+        cls.tracer_provider: TracerProvider = get_tracer_provider()
 
+    # Sanity check that the trace ID ratio sampler works fine with the x-ray generator.
     def test_provide_generate_xray_ids(self):
         for _ in range(20):
             tracer: Tracer = self.tracer_provider.get_tracer("test")
             start_time_sec: int = int(time.time())
             span: Span = tracer.start_span("test")
             trace_id: int = span.get_span_context().trace_id
-            self.assertGreater(trace_id, start_time_sec)
+            trace_id_4_byte_hex: str = hex(trace_id)[2:10]
+            trace_id_4_byte_int: int = int(trace_id_4_byte_hex, 16)
+            self.assertGreaterEqual(trace_id_4_byte_int, start_time_sec)
 
+    # Sanity check that the trace ID ratio sampler works fine with the x-ray generator.
     def test_trace_id_ratio_sampler(self):
         for _ in range(20):
-            num_spans: int = 10000
+            num_spans: int = 100000
             num_sampled: int = 0
             tracer: Tracer = self.tracer_provider.get_tracer("test")
             for _ in range(num_spans):
@@ -47,4 +52,5 @@ class TestAwsTracerConfigurer(TestCase):
                 if span.get_span_context().trace_flags.sampled:
                     num_sampled += 1
                 span.end()
+        # Configured for 1%, confirm there are at most 5% to account for randomness and reduce test flakiness.
             self.assertGreater(0.05, num_sampled / num_spans)
