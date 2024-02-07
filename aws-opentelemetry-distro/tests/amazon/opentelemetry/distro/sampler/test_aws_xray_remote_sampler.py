543792fd20648c81dd29621fe84b92110cb2cb5c
Jonathan Lee <107072447+jj22ee@users.noreply.github.com>
2024-02-06 17:22:53 -0800

    AWS X-Ray Remote Sampler Part 1 - Initial Rules Poller Implementation (#33)
    *Issue #, if available:*
    First PR of 3 parts for adding the X-Ray remote sampling support for
    OTel Python SDK.
    
    *Description of changes:*
    - Python Classes
    - `AwsXRayRemoteSampler` - extends
    `opentelemetry.sdk.trace.sampling.Sampler` and implements
    `should_sample`.
    - Upon initialization, starts polling for sampling rules by scheduling a
    threading.Timer to execute a poll after a configurable interval of time.
    After this interval, it will repeat this process indefinitely by
    scheduling the same threading.Timer upon completion of the previous
    timer.
    - OTel `resource`, Collector `endpoint`, rules `polling_interval` are
    configurable.
      - `AwsXRaySamplingClient` - client to call GetSamplingRules
      - `SamplingRule` - Class for SamplingRules type
    
    Testing Script to poll Sampling Rules every 5 seconds:
    ```
    import logging
    import time
    
    from amazon.opentelemetry.distro.sampler.aws_xray_remote_sampler import AwsXRayRemoteSampler
    from opentelemetry.sdk.resources import Resource
    
    logging.basicConfig(level=logging.INFO)
    sampler = AwsXRayRemoteSampler(Resource.get_empty(), polling_interval=5)
    
    time.sleep(15)
    ```
    
    Output:
    ```
    88665a53c0dd:sampler jjllee$ python3 mytesting.py 
    INFO:amazon.opentelemetry.distro.sampler.aws_xray_remote_sampler:Got Sampling Rules: {'[{"Attributes": {}, "FixedRate": 0.05, "HTTPMethod": "*", "Host": "*", "Priority": 10000, "ReservoirSize": 100, "ResourceARN": "*", "RuleARN": "arn:aws:xray:us-east-1:999999999999:sampling-rule/Default", "RuleName": "Default", "ServiceName": "*", "ServiceType": "*", "URLPath": "*", "Version": 1}, {"Attributes": {"abc": "1234"}, "FixedRate": 0.11, "HTTPMethod": "*", "Host": "*", "Priority": 20, "ReservoirSize": 1, "ResourceARN": "*", "RuleARN": "arn:aws:xray:us-east-1:999999999999:sampling-rule/test", "RuleName": "test", "ServiceName": "*", "ServiceType": "*", "URLPath": "*", "Version": 1}]'}
    INFO:amazon.opentelemetry.distro.sampler.aws_xray_remote_sampler:Got Sampling Rules: {'[{"Attributes": {}, "FixedRate": 0.05, "HTTPMethod": "*", "Host": "*", "Priority": 10000, "ReservoirSize": 100, "ResourceARN": "*", "RuleARN": "arn:aws:xray:us-east-1:999999999999:sampling-rule/Default", "RuleName": "Default", "ServiceName": "*", "ServiceType": "*", "URLPath": "*", "Version": 1}, {"Attributes": {"abc": "1234"}, "FixedRate": 0.11, "HTTPMethod": "*", "Host": "*", "Priority": 20, "ReservoirSize": 1, "ResourceARN": "*", "RuleARN": "arn:aws:xray:us-east-1:999999999999:sampling-rule/test", "RuleName": "test", "ServiceName": "*", "ServiceType": "*", "URLPath": "*", "Version": 1}]'}
    INFO:amazon.opentelemetry.distro.sampler.aws_xray_remote_sampler:Got Sampling Rules: {'[{"Attributes": {}, "FixedRate": 0.05, "HTTPMethod": "*", "Host": "*", "Priority": 10000, "ReservoirSize": 100, "ResourceARN": "*", "RuleARN": "arn:aws:xray:us-east-1:999999999999:sampling-rule/Default", "RuleName": "Default", "ServiceName": "*", "ServiceType": "*", "URLPath": "*", "Version": 1}, {"Attributes": {"abc": "1234"}, "FixedRate": 0.11, "HTTPMethod": "*", "Host": "*", "Priority": 20, "ReservoirSize": 1, "ResourceARN": "*", "RuleARN": "arn:aws:xray:us-east-1:999999999999:sampling-rule/test", "RuleName": "test", "ServiceName": "*", "ServiceType": "*", "URLPath": "*", "Version": 1}]'}
    88665a53c0dd:sampler jjllee$ 
    ```
    
    
    
    
    
    
    By submitting this pull request, I confirm that you can use, modify,
    copy, and redistribute this contribution, under the terms of your
    choice.
    
    ---------
    
    Co-authored-by: Prashant Srivastava <50466688+srprash@users.noreply.github.com>

---
+++ aws-opentelemetry-distro/tests/amazon/opentelemetry/distro/sampler/test_aws_xray_remote_sampler.py
@@ -0,0 +1,45 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+from logging import DEBUG
+from unittest import TestCase
+
+from amazon.opentelemetry.distro.sampler.aws_xray_remote_sampler import AwsXRayRemoteSampler
+from opentelemetry.sdk.resources import Resource
+
+
+class TestAwsXRayRemoteSampler(TestCase):
+    def test_create_remote_sampler_with_empty_resource(self):
+        rs = AwsXRayRemoteSampler(resource=Resource.get_empty())
+        self.assertIsNotNone(rs._timer)
+        self.assertEqual(rs._AwsXRayRemoteSampler__polling_interval, 300)
+        self.assertIsNotNone(rs._AwsXRayRemoteSampler__xray_client)
+        self.assertIsNotNone(rs._AwsXRayRemoteSampler__resource)
+
+    def test_create_remote_sampler_with_populated_resource(self):
+        rs = AwsXRayRemoteSampler(
+            resource=Resource.create({"service.name": "test-service-name", "cloud.platform": "test-cloud-platform"})
+        )
+        self.assertIsNotNone(rs._timer)
+        self.assertEqual(rs._AwsXRayRemoteSampler__polling_interval, 300)
+        self.assertIsNotNone(rs._AwsXRayRemoteSampler__xray_client)
+        self.assertIsNotNone(rs._AwsXRayRemoteSampler__resource)
+        self.assertEqual(rs._AwsXRayRemoteSampler__resource.attributes["service.name"], "test-service-name")
+        self.assertEqual(rs._AwsXRayRemoteSampler__resource.attributes["cloud.platform"], "test-cloud-platform")
+
+    def test_create_remote_sampler_with_all_fields_populated(self):
+        rs = AwsXRayRemoteSampler(
+            resource=Resource.create({"service.name": "test-service-name", "cloud.platform": "test-cloud-platform"}),
+            endpoint="http://abc.com",
+            polling_interval=120,
+            log_level=DEBUG,
+        )
+        self.assertIsNotNone(rs._timer)
+        self.assertEqual(rs._AwsXRayRemoteSampler__polling_interval, 120)
+        self.assertIsNotNone(rs._AwsXRayRemoteSampler__xray_client)
+        self.assertIsNotNone(rs._AwsXRayRemoteSampler__resource)
+        self.assertEqual(
+            rs._AwsXRayRemoteSampler__xray_client._AwsXRaySamplingClient__get_sampling_rules_endpoint,
+            "http://abc.com/GetSamplingRules",
+        )
+        self.assertEqual(rs._AwsXRayRemoteSampler__resource.attributes["service.name"], "test-service-name")
+        self.assertEqual(rs._AwsXRayRemoteSampler__resource.attributes["cloud.platform"], "test-cloud-platform")
