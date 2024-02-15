fcf00edbb2a1c3cb3668c09b32b04189f8f095b9
Zhonghao Zhao <zzhlogin@amazon.com>
2024-02-14 20:00:49 -0800

    Apply lint

--- aws-opentelemetry-distro/src/amazon/opentelemetry/distro/_aws_attribute_keys.py
+++ aws-opentelemetry-distro/src/amazon/opentelemetry/distro/_aws_attribute_keys.py
@@ -15,8 +15,8 @@ AWS_CONSUMER_PARENT_SPAN_KIND: str = "aws.consumer.parent.span.kind"
 # AWS_#_NAME attributes are not supported in python as they are not part of the Semantic Conventions.
 # Use SpanAttributes for AWS_TABLE_NAME and AWS_BUCKET_NAME.
 # TODO：Move to Semantic Conventions when these attributes are added.
-AWS_TABLE_NAME: str = SpanAttributes.AWS_DYNAMODB_TABLE_NAMES  # "aws.table.name" -> "aws.dynamodb.table_names"
-AWS_BUCKET_NAME: str = SpanAttributes.AWS_S3_BUCKET  # "aws.bucket.name" -> "aws.s3.bucket"
+AWS_TABLE_NAME: str = SpanAttributes.AWS_DYNAMODB_TABLE_NAMES
+AWS_BUCKET_NAME: str = SpanAttributes.AWS_S3_BUCKET
 AWS_QUEUE_URL: str = "aws.queue.url"
 AWS_QUEUE_NAME: str = "aws.queue.name"
 AWS_STREAM_NAME: str = "aws.stream.name"
