d1033927a3b2dc56b4134bd8c2b361236a574d97
Thomas Pierce <thp@amazon.com>
2024-02-26 18:24:25 +0000

    Copy benchmark-overhead from opentelemetry-java-instrumentation to performance-tests
    In this commit we are copying all content from https://github.com/open-telemetry/opentelemetry-java-instrumentation/tree/main/benchmark-overhead to https://github.com/aws-observability/aws-otel-python-instrumentation/tree/main/performance-tests. We will be using benchmark-overhead as a template to building our own performance tests, so initially we are just copying all content over. Much of this content will be changed and/or deleted in upcoming commits, but for now, it's just a raw copy-paste.

---
+++ performance-tests/src/test/java/io/opentelemetry/config/Configs.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.config;
+
+import io.opentelemetry.agents.Agent;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+/** Defines all test configurations */
+public enum Configs {
+  RELEASE(
+      TestConfig.builder()
+          .name("release")
+          .description("compares no agent, latest stable, and latest snapshot agents")
+          .withAgents(Agent.NONE, Agent.LATEST_RELEASE, Agent.LATEST_SNAPSHOT)
+          .warmupSeconds(60)
+          .build());
+
+  public final TestConfig config;
+
+  public static Stream<TestConfig> all() {
+    return Arrays.stream(Configs.values()).map(x -> x.config);
+  }
+
+  Configs(TestConfig config) {
+    this.config = config;
+  }
+}
