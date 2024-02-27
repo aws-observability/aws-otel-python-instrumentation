9a25f1156198526e64e7c6b8171b69b85c8cfb59
Mohamed Asaker <asakem@amazon.com>
2024-02-27 11:19:48 -0800

    created migration file

--- sample-applications/vehicle-dealership-sample-app/VehicleInventoryApp/MainService/url.py
+++ sample-applications/vehicle-dealership-sample-app/VehicleInventoryApp/MainService/url.py
@@ -7,7 +7,7 @@ from . import views
 urlpatterns = [
     path("", views.vehicle, name="vehicle"),
     path("<int:vehicle_id>", views.get_vehicle_by_id, name="get_vehicle_by_id"),
-    path("name/<str:vehicles_name>", views.get_vehicles_by_name(), name="get_vehicles_by_name"),
+    path("name/<str:vehicles_name>", views.get_vehicles_by_name, name="get_vehicles_by_name"),
     path("<int:vehicle_id>/image", views.get_vehicle_image, name="get_vehicle_image"),
     path("image/<str:image_name>", views.image, name="image"),
     path("history/", views.vehicle_purchase_history, name="purchase_history"),
