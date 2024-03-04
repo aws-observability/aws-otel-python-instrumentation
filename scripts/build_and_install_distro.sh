74a96ec51e0da5a83bd3d192b14fa4bca5829a6e
Thomas Pierce <thp@amazon.com>
2024-03-04 15:16:48 -0800

    Make build script more robust

--- scripts/build_and_install_distro.sh
+++ scripts/build_and_install_distro.sh
@@ -14,7 +14,7 @@ if [ "$current_dir" != "aws-otel-python-instrumentation" ]; then
 fi
 
 # Setup - update dependencies and create/empty dist dir
-pip install --upgrade pip setuptools wheel packaging build
+python3 -m pip install --upgrade pip setuptools wheel packaging build
 mkdir -p dist
 rm -rf dist/aws_opentelemetry_distro*
 
@@ -25,5 +25,5 @@ python3 -m build --outdir ../dist
 # Install distro
 cd ../dist
 DISTRO=(aws_opentelemetry_distro-*-py3-none-any.whl)
-pip install $DISTRO --force-reinstall
+python3 -m pip install $DISTRO --force-reinstall
 cd ..
