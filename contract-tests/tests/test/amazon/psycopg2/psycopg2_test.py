1d4901c5d776a4586943784d407b2607d73bdd63
ADOT Patch workflow <adot-patch-workflow@github.com>
2024-03-04 13:59:08 -0800

    Adjust based on comments

--- contract-tests/tests/test/amazon/psycopg2/psycopg2_test.py
+++ contract-tests/tests/test/amazon/psycopg2/psycopg2_test.py
@@ -111,6 +111,7 @@ class Psycopg2Test(ContractTestBase):
         self.assertTrue(attributes_dict.get("db.statement").string_value.startswith(command))
         self._assert_str_attribute(attributes_dict, "db.system", "postgresql")
         self._assert_str_attribute(attributes_dict, "db.name", "postgres")
+        self.assertTrue("db.operation" not in attributes_dict.keys())
 
     @override
     def _assert_metric_attribute(
