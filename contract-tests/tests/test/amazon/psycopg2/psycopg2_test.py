c30284ef8071f03ed216403afcadabf25ad4860b
ADOT Patch workflow <adot-patch-workflow@github.com>
2024-03-04 15:09:41 -0800

    change the way test implemented

--- contract-tests/tests/test/amazon/psycopg2/psycopg2_test.py
+++ contract-tests/tests/test/amazon/psycopg2/psycopg2_test.py
@@ -104,8 +104,7 @@ class Psycopg2Test(ContractTestBase):
 
         self.assertEqual(target_spans[0].name, kwargs.get("sql_command").split()[0])
         if status_code == 200:
-            print(type(target_spans[0].status))
-            self.assertEqual(target_spans[0].status, [])
+            self.assertEqual(target_spans[0].status.code, StatusCode.UNSET.value)
         else:
             self.assertEqual(target_spans[0].status.code, StatusCode.ERROR.value)
 
