823590df225e4142a8cfd357136f40dcf05086e2
XinRan Zhang <xinranzh@amazon.com>
2024-04-12 10:03:55 -0700

    A init but should functional ver

--- contract-tests/tests/test/amazon/misc/service_name_in_resource_attributes_test.py
+++ contract-tests/tests/test/amazon/misc/service_name_in_resource_attributes_test.py
@@ -5,7 +5,7 @@ from typing import Dict, List
 from mock_collector_client import ResourceScopeMetric, ResourceScopeSpan
 from typing_extensions import override
 
-from amazon.base.resource_attributes_test_base import ResourceAttributesTest
+from resource_attributes_test_base import ResourceAttributesTest
 from amazon.utils.app_signals_constants import AWS_LOCAL_OPERATION, AWS_LOCAL_SERVICE, AWS_SPAN_KIND
 from opentelemetry.proto.common.v1.common_pb2 import AnyValue, KeyValue
 from opentelemetry.proto.metrics.v1.metrics_pb2 import ExponentialHistogramDataPoint, Metric
@@ -19,5 +19,5 @@ class ServiceNameInResourceAttributesTest(ResourceAttributesTest):
     def get_application_otel_resource_attributes(self):
         return "service.name=service-name"
 
-    def test_service_name_in_resource_attributes(self):
+    def test_service(self):
         self.assert_resource_attributes("service-name")
