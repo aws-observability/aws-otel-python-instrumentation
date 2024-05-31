2db7a0007a5803051d0a5bc0382fbb3c27a70e25
Mohamed Asaker <asakermohamad471@icloud.com>
2024-05-31 13:52:17 -0700

    Deleted the Vehicle Sample App (#198)
    Deleted the Vehicle Sample App as it's no longer required. 
    
    By submitting this pull request, I confirm that you can use, modify,
    copy, and redistribute this contribution, under the terms of your
    choice.

--- sample-applications/vehicle-dealership-sample-app/VehicleInventoryApp/MainService/apps.py
+++
@@ -1,8 +0,0 @@
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: Apache-2.0
-from django.apps import AppConfig
-
-
-class MainServiceConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "MainService"
