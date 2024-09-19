b6cf2fc677ca74e5a695e80de630b442c279966f
Wang <wangzl@amazon.com>
2024-09-18 18:19:34 -0700

    Changing span unsampled flag to be sampled flag

--- aws-opentelemetry-distro/src/amazon/opentelemetry/distro/aws_batch_unsampled_span_processor.py
+++ aws-opentelemetry-distro/src/amazon/opentelemetry/distro/aws_batch_unsampled_span_processor.py
@@ -3,14 +3,12 @@
 import logging
 from typing import Optional
 
-from amazon.opentelemetry.distro._aws_attribute_keys import AWS_TRACE_FLAG_UNSAMPLED
+from amazon.opentelemetry.distro._aws_attribute_keys import AWS_TRACE_FLAG_SAMPLED
 from opentelemetry.context import Context
 from opentelemetry.sdk.trace import ReadableSpan, Span
 from opentelemetry.sdk.trace.export import BatchSpanProcessor as BaseBatchSpanProcessor
 
 logger = logging.getLogger(__name__)
-
-SPANUNSAMPLED_FLAG = "OTEL_AWS_APP_SIGNALS_ENABLED"
 
 
 class BatchUnsampledSpanProcessor(BaseBatchSpanProcessor):
@@ -18,7 +16,7 @@ class BatchUnsampledSpanProcessor(BaseBatchSpanProcessor):
     # pylint: disable=no-self-use
     def on_start(self, span: Span, parent_context: Optional[Context] = None) -> None:
         if not span.context.trace_flags.sampled:
-            span.set_attribute(AWS_TRACE_FLAG_UNSAMPLED, True)
+            span.set_attribute(AWS_TRACE_FLAG_SAMPLED, False)
 
     def on_end(self, span: ReadableSpan) -> None:
         if span.context.trace_flags.sampled:
