78cd83bede3747481dce578558c305f6bdaecbee
liustve <liustve@amazon.com>
2024-12-12 22:40:05 +0000

    revert bedrock testing on simple client server app

--- sample-applications/simple-client-server/server_automatic_s3client.py
+++ sample-applications/simple-client-server/server_automatic_s3client.py
@@ -1,29 +1,20 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 import boto3
-import json
-from flask import Flask
+from flask import Flask, request
 
-client = boto3.client(service_name="bedrock-runtime")
+# Let's use Amazon S3
+s3 = boto3.resource("s3")
+
 app = Flask(__name__)
+
 
 @app.route("/server_request")
 def server_request():
-    messages = [
-    {"role": "user", "content": [{"text": "Write a short poem"}]},
- ]
-
-    model_response = client.converse(
-        modelId="us.amazon.nova-lite-v1:0", 
-        messages=messages
-    )
-
-    print("\n[Full Response]")
-    print(json.dumps(model_response, indent=2))
-
-    print("\n[Response Content Text]")
-    print(model_response["output"]["message"]["content"][0]["text"])
-    
+    print(request.args.get("param"))
+    for bucket in s3.buckets.all():
+        print(bucket.name)
+    return "served"
 
 
 if __name__ == "__main__":
