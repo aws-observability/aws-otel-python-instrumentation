8644e082766886daae9ec3fc96ac2feca596deab
Michael He <53622546+yiyuan-he@users.noreply.github.com>
2025-06-10 16:22:31 -0700

    Configure Span Pipeline for Genesis (#380)
    ## What does this pull request do?
    Configures span pipeline for Genesis to send traces to OTLP X-Ray
    endpoint when `AGENT_OBSERVABILITY_ENABLED` is enabled. We also pass an
    instance of `logger_provider` to our custom OTLP Span Exporter since we
    may need to [emit input/output responses to the logs
    pipeline](https://opentelemetry.io/docs/specs/semconv/gen-ai/gen-ai-events/).
    
    By submitting this pull request, I confirm that you can use, modify,
    copy, and redistribute this contribution, under the terms of your
    choice.

--- aws-opentelemetry-distro/src/amazon/opentelemetry/distro/_utils.py
+++ aws-opentelemetry-distro/src/amazon/opentelemetry/distro/_utils.py
@@ -1,12 +1,15 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
+import os
 import sys
 from logging import Logger, getLogger
 
 import pkg_resources
 
 _logger: Logger = getLogger(__name__)
+
+AGENT_OBSERVABILITY_ENABLED = "AGENT_OBSERVABILITY_ENABLED"
 
 
 def is_installed(req: str) -> bool:
@@ -21,3 +24,8 @@ def is_installed(req: str) -> bool:
         _logger.debug("Skipping instrumentation patch: package %s, exception: %s", req, exc)
         return False
     return True
+
+
+def is_agent_observability_enabled() -> bool:
+    """Is the Agentic AI monitoring flag set to true?"""
+    return os.environ.get(AGENT_OBSERVABILITY_ENABLED, "false").lower() == "true"
