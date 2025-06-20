b75fe99545d4ab092c66ef09f534f26c6c1d1644
liustve <liustve@amazon.com>
2025-06-20 00:19:50 +0000

    linting fix

--- aws-opentelemetry-distro/tests/amazon/opentelemetry/distro/exporter/otlp/aws/logs/test_otlp_aws_logs_exporter.py
+++ aws-opentelemetry-distro/tests/amazon/opentelemetry/distro/exporter/otlp/aws/logs/test_otlp_aws_logs_exporter.py
@@ -1,3 +1,5 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
 import time
 from unittest import TestCase
 from unittest.mock import patch
