c7a4af5abfc9867136fdeaab8aafd2275b4a12ef
mxiamxia@gmail.com <mxiamxia@gmail.com>
2025-06-23 10:21:06 -0700

    Fix race condition issue on ASGI framework which main thread and worker thread competeting to each other

--- aws-opentelemetry-distro/src/amazon/opentelemetry/distro/exporter/otlp/aws/logs/aws_batch_log_record_processor.py
+++ aws-opentelemetry-distro/src/amazon/opentelemetry/distro/exporter/otlp/aws/logs/aws_batch_log_record_processor.py
@@ -165,3 +165,12 @@ class AwsBatchLogRecordProcessor(BatchLogRecordProcessor):
         }
 
         return log.instrumentation_scope.name in gen_ai_instrumentations
+    
+    # Only export the logs once to avoid the race condition of the worker thread and force flush thread
+    # https://github.com/open-telemetry/opentelemetry-python/issues/3193
+    # https://github.com/open-telemetry/opentelemetry-python/blob/main/opentelemetry-sdk/src/opentelemetry/sdk/_shared_internal/__init__.py#L199
+    def force_flush(self, timeout_millis: Optional[int] = None) -> bool:
+        if self._shutdown:
+            return False 
+        self._export(BatchLogExportStrategy.EXPORT_AT_LEAST_ONE_BATCH)
+        return True
