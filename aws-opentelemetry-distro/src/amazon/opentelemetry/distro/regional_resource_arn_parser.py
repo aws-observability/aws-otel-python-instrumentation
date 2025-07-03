8ac666a121ee572c00520c29a9d38b398f462ffb
Blair Huang <blairhyy@amazon.com>
2025-07-02 20:55:40 -0700

    refactor arn parser

--- aws-opentelemetry-distro/src/amazon/opentelemetry/distro/regional_resource_arn_parser.py
+++ aws-opentelemetry-distro/src/amazon/opentelemetry/distro/regional_resource_arn_parser.py
@@ -8,48 +8,32 @@ from amazon.opentelemetry.distro._utils import is_account_id
 class RegionalResourceArnParser:
     @staticmethod
     def get_account_id(arn: str) -> Optional[str]:
-        if _is_arn(arn):
-            return str(arn).split(":")[4]
-        return None
+        parts = _get_arn_parts(arn)
+        return parts[4] if parts else None
 
     @staticmethod
     def get_region(arn: str) -> Optional[str]:
-        if _is_arn(arn):
-            return str(arn).split(":")[3]
-        return None
+        parts = _get_arn_parts(arn)
+        return parts[3] if parts else None
 
     @staticmethod
     def extract_dynamodb_table_name_from_arn(arn: str) -> Optional[str]:
-        resource_name = RegionalResourceArnParser.extract_resource_name_from_arn(arn)
-        if resource_name:
-            return resource_name.replace("table/", "")
-        return None
+        parts = _get_arn_parts(arn)
+        return parts[-1].replace("table/", "") if parts else None
 
     @staticmethod
     def extract_kinesis_stream_name_from_arn(arn: str) -> Optional[str]:
-        resource_name = RegionalResourceArnParser.extract_resource_name_from_arn(arn)
-        if resource_name:
-            return resource_name.replace("stream/", "")
-        return None
+        parts = _get_arn_parts(arn)
+        return parts[-1].replace("stream/", "") if parts else None
 
     @staticmethod
     def extract_resource_name_from_arn(arn: str) -> Optional[str]:
-        # Extracts the name of the resource from an arn
-        if _is_arn(arn):
-            split = arn.split(":")
-            return split[-1]
+        parts = _get_arn_parts(arn)
+        return parts[-1] if parts else None
+
+
+def _get_arn_parts(arn: str) -> Optional[list]:
+    if not arn or not arn.startswith("arn"):
         return None
-
-
-def _is_arn(arn: str) -> bool:
-    # Check if arn follows the format:
-    # arn:partition:service:region:account-id:resource-type/resource-id or
-    # arn:partition:service:region:account-id:resource-type:resource-id
-    if arn is None:
-        return False
-
-    if not str(arn).startswith("arn"):
-        return False
-
-    arn_parts = str(arn).split(":")
-    return len(arn_parts) >= 6 and is_account_id(arn_parts[4])
+    parts = arn.split(":")
+    return parts if len(parts) >= 6 and is_account_id(parts[4]) else None
