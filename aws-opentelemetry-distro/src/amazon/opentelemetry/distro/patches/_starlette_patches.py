1e35cc6e34bcb76eeec6f02e26aa96a5c23cbd05
Steve Liu <liustve@amazon.com>
2025-08-29 12:56:15 -0700

    add patch for excluding asgi event spans from starlette

--- aws-opentelemetry-distro/src/amazon/opentelemetry/distro/patches/_starlette_patches.py
+++ aws-opentelemetry-distro/src/amazon/opentelemetry/distro/patches/_starlette_patches.py
@@ -4,7 +4,7 @@
 from logging import Logger, getLogger
 from typing import Collection
 
-from amazon.opentelemetry.distro._utils import AGENT_OBSERVABILITY_ENABLED
+from amazon.opentelemetry.distro._utils import is_agent_observability_enabled
 
 _logger: Logger = getLogger(__name__)
 
@@ -31,10 +31,12 @@ def _apply_starlette_instrumentation_patches() -> None:
         # Apply the patch
         StarletteInstrumentor.instrumentation_dependencies = patched_instrumentation_dependencies
 
-        # Patch to exclude http receive/send ASGI event spans, this Middleware instrumentation is injected
-        # internally by Starlette Instrumentor, see:
+        # Patch to exclude http receive/send ASGI event spans from Bedrock AgentCore,
+        # this Middleware instrumentation is injected internally by Starlette Instrumentor, see:
         # https://github.com/open-telemetry/opentelemetry-python-contrib/blob/51da0a766e5d3cbc746189e10c9573163198cfcd/instrumentation/opentelemetry-instrumentation-asgi/src/opentelemetry/instrumentation/asgi/__init__.py#L573
-        if AGENT_OBSERVABILITY_ENABLED:
+        #
+        # Issue for tracking a feature to customize this setting within Starlette: https://github.com/open-telemetry/opentelemetry-python-contrib/issues/3725
+        if is_agent_observability_enabled():
             original_init = OpenTelemetryMiddleware.__init__
 
             def patched_init(self, app, **kwargs):
