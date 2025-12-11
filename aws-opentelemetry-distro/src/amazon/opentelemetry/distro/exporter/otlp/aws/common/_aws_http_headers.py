b20565e665dcdc792c1fd6998db95ec142546af3
Steve Liu <liustve@amazon.com>
2025-12-11 15:57:42 -0800

    lint fix

--- aws-opentelemetry-distro/src/amazon/opentelemetry/distro/exporter/otlp/aws/common/_aws_http_headers.py
+++ aws-opentelemetry-distro/src/amazon/opentelemetry/distro/exporter/otlp/aws/common/_aws_http_headers.py
@@ -7,10 +7,9 @@ from amazon.opentelemetry.distro.version import __version__
 
 def build_user_agent() -> str:
     user_agent = f"ADOT-OTLP-Exporter-Python/{__version__}"
-
+    
     if is_agent_observability_enabled():
         user_agent = f"ADOT-GenAI-OTLP-Exporter-Python/{__version__}"
-
     return user_agent
 
 
