d1a8b30d2d9c12ca24564cb21cf54e570d928900
Steve Liu <liustve@amazon.com>
2026-02-05 15:20:24 -0800

    fix comment

--- aws-opentelemetry-distro/src/amazon/opentelemetry/distro/semconv/_incubating/attributes/gen_ai_attributes.py
+++ aws-opentelemetry-distro/src/amazon/opentelemetry/distro/semconv/_incubating/attributes/gen_ai_attributes.py
@@ -1,9 +1,8 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 """
-Utility module holding attribute keys for incubating Gen AI semantic conventions
-not yet available in opentelemetry-semantic-conventions 0.54b1.
-Remove attributes once they are contributed to upstream.
+Utility module holding attribute keys for incubating Gen AI semantic conventions.
+Remove this once we've contributed them to upstream.
 """
 
 GEN_AI_RUNTIME_ID = "gen_ai.runtime.id"
