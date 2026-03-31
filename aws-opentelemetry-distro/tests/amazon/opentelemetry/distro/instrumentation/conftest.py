8c42d2373e206fe2d829adbc2e6d00b9b5285338
Steve Liu <liustve@amazon.com>
2026-03-31 12:31:18 -0700

    fix: lazy import jsonschema to avoid collection error on py3.9

--- aws-opentelemetry-distro/tests/amazon/opentelemetry/distro/instrumentation/conftest.py
+++ aws-opentelemetry-distro/tests/amazon/opentelemetry/distro/instrumentation/conftest.py
@@ -4,13 +4,13 @@
 import json
 import urllib.request
 
-import jsonschema
-
 _OTEL_SCHEMA_BASE = "https://opentelemetry.io/docs/specs/semconv"
 _SCHEMA_CACHE: dict = {}
 
 
 def validate_otel_schema(data, schema_url: str) -> None:
+    import jsonschema
+
     if schema_url not in _SCHEMA_CACHE:
         with urllib.request.urlopen(schema_url) as resp:
             _SCHEMA_CACHE[schema_url] = json.loads(resp.read())
