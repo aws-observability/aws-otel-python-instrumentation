1bc6a2e839144fa9d30a259448efdd3c1bacdfcd
Steve Liu <liustve@amazon.com>
2026-08-12 15:50:22 -0700

    refactor: label incubating semconv module paths

--- cloudwatch-plugin-otel/src/plugins/opentelemetry/cloudwatch/span_metrics/_constants.py
+++ cloudwatch-plugin-otel/src/plugins/opentelemetry/cloudwatch/span_metrics/_constants.py
@@ -22,40 +22,40 @@ HTTP_REQUEST_METHOD = _semconv(_HTTP, "HTTP_REQUEST_METHOD", "http.request.metho
 HTTP_RESPONSE_STATUS_CODE = _semconv(_HTTP, "HTTP_RESPONSE_STATUS_CODE", "http.response.status_code")
 HTTP_ROUTE = _semconv(_HTTP, "HTTP_ROUTE", "http.route")
 
-_HTTP_LEGACY = "opentelemetry.semconv._incubating.attributes.http_attributes"
-HTTP_METHOD = _semconv(_HTTP_LEGACY, "HTTP_METHOD", "http.method")
-HTTP_STATUS_CODE = _semconv(_HTTP_LEGACY, "HTTP_STATUS_CODE", "http.status_code")
+_HTTP_INCUBATING = "opentelemetry.semconv._incubating.attributes.http_attributes"
+HTTP_METHOD = _semconv(_HTTP_INCUBATING, "HTTP_METHOD", "http.method")
+HTTP_STATUS_CODE = _semconv(_HTTP_INCUBATING, "HTTP_STATUS_CODE", "http.status_code")
 
 _ERROR = "opentelemetry.semconv.attributes.error_attributes"
 ERROR_TYPE = _semconv(_ERROR, "ERROR_TYPE", "error.type")
 
-_RPC = "opentelemetry.semconv._incubating.attributes.rpc_attributes"
-RPC_SYSTEM_NAME = _semconv(_RPC, "RPC_SYSTEM_NAME", "rpc.system.name")
-RPC_SYSTEM = _semconv(_RPC, "RPC_SYSTEM", "rpc.system")
-RPC_SERVICE = _semconv(_RPC, "RPC_SERVICE", "rpc.service")
-RPC_METHOD = _semconv(_RPC, "RPC_METHOD", "rpc.method")
+_RPC_INCUBATING = "opentelemetry.semconv._incubating.attributes.rpc_attributes"
+RPC_SYSTEM_NAME = _semconv(_RPC_INCUBATING, "RPC_SYSTEM_NAME", "rpc.system.name")
+RPC_SYSTEM = _semconv(_RPC_INCUBATING, "RPC_SYSTEM", "rpc.system")
+RPC_SERVICE = _semconv(_RPC_INCUBATING, "RPC_SERVICE", "rpc.service")
+RPC_METHOD = _semconv(_RPC_INCUBATING, "RPC_METHOD", "rpc.method")
 
-_DB = "opentelemetry.semconv._incubating.attributes.db_attributes"
-DB_SYSTEM_NAME = _semconv(_DB, "DB_SYSTEM_NAME", "db.system.name")
-DB_SYSTEM = _semconv(_DB, "DB_SYSTEM", "db.system")
-DB_OPERATION_NAME = _semconv(_DB, "DB_OPERATION_NAME", "db.operation.name")
-DB_OPERATION = _semconv(_DB, "DB_OPERATION", "db.operation")
-DB_COLLECTION_NAME = _semconv(_DB, "DB_COLLECTION_NAME", "db.collection.name")
-DB_SQL_TABLE = _semconv(_DB, "DB_SQL_TABLE", "db.sql.table")
-DB_MONGODB_COLLECTION = _semconv(_DB, "DB_MONGODB_COLLECTION", "db.mongodb.collection")
-DB_CASSANDRA_TABLE = _semconv(_DB, "DB_CASSANDRA_TABLE", "db.cassandra.table")
-DB_COSMOSDB_CONTAINER = _semconv(_DB, "DB_COSMOSDB_CONTAINER", "db.cosmosdb.container")
+_DB_INCUBATING = "opentelemetry.semconv._incubating.attributes.db_attributes"
+DB_SYSTEM_NAME = _semconv(_DB_INCUBATING, "DB_SYSTEM_NAME", "db.system.name")
+DB_SYSTEM = _semconv(_DB_INCUBATING, "DB_SYSTEM", "db.system")
+DB_OPERATION_NAME = _semconv(_DB_INCUBATING, "DB_OPERATION_NAME", "db.operation.name")
+DB_OPERATION = _semconv(_DB_INCUBATING, "DB_OPERATION", "db.operation")
+DB_COLLECTION_NAME = _semconv(_DB_INCUBATING, "DB_COLLECTION_NAME", "db.collection.name")
+DB_SQL_TABLE = _semconv(_DB_INCUBATING, "DB_SQL_TABLE", "db.sql.table")
+DB_MONGODB_COLLECTION = _semconv(_DB_INCUBATING, "DB_MONGODB_COLLECTION", "db.mongodb.collection")
+DB_CASSANDRA_TABLE = _semconv(_DB_INCUBATING, "DB_CASSANDRA_TABLE", "db.cassandra.table")
+DB_COSMOSDB_CONTAINER = _semconv(_DB_INCUBATING, "DB_COSMOSDB_CONTAINER", "db.cosmosdb.container")
 
-_MESSAGING = "opentelemetry.semconv._incubating.attributes.messaging_attributes"
-MESSAGING_SYSTEM = _semconv(_MESSAGING, "MESSAGING_SYSTEM", "messaging.system")
-MESSAGING_OPERATION_NAME = _semconv(_MESSAGING, "MESSAGING_OPERATION_NAME", "messaging.operation.name")
-MESSAGING_DESTINATION_NAME = _semconv(_MESSAGING, "MESSAGING_DESTINATION_NAME", "messaging.destination.name")
+_MESSAGING_INCUBATING = "opentelemetry.semconv._incubating.attributes.messaging_attributes"
+MESSAGING_SYSTEM = _semconv(_MESSAGING_INCUBATING, "MESSAGING_SYSTEM", "messaging.system")
+MESSAGING_OPERATION_NAME = _semconv(_MESSAGING_INCUBATING, "MESSAGING_OPERATION_NAME", "messaging.operation.name")
+MESSAGING_DESTINATION_NAME = _semconv(_MESSAGING_INCUBATING, "MESSAGING_DESTINATION_NAME", "messaging.destination.name")
 MESSAGING_DESTINATION = "messaging.destination"
 MESSAGING_DESTINATION_TEMPORARY = _semconv(
-    _MESSAGING, "MESSAGING_DESTINATION_TEMPORARY", "messaging.destination.temporary"
+    _MESSAGING_INCUBATING, "MESSAGING_DESTINATION_TEMPORARY", "messaging.destination.temporary"
 )
 MESSAGING_DESTINATION_ANONYMOUS = _semconv(
-    _MESSAGING, "MESSAGING_DESTINATION_ANONYMOUS", "messaging.destination.anonymous"
+    _MESSAGING_INCUBATING, "MESSAGING_DESTINATION_ANONYMOUS", "messaging.destination.anonymous"
 )
 
 
